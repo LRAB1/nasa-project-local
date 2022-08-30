0a39e03035b15d66820367ab5f8ffbd9f4fbb54c
LRAB1 <110101676+LRAB1@users.noreply.github.com>
2022-08-30 16:32:57 +0200

    preparing launches routing etc

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,8 @@
 //const http = require('https');
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
-path = require('path')
+const morgan = require('morgan');
 
 const planetsRouter = require('./routes/planets/planets.router');
 
@@ -10,6 +11,7 @@ const app = express();
 app.use(cors({
     origin: 'http://localhost:3000'
 }));
+app.use(morgan('combined'));
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')))

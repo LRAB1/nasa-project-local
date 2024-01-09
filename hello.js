a9e6eeb90f9fef7e5ad7c8275caf3bb07b146952
LRAB1 <lexbant@gmail.com>
2024-01-09 12:57:21 +0100

    funneh

--- hello.js
+++ hello.js
@@ -1,6 +1,6 @@
-const mission = 'learn';
+const mission = process.argv[2];
 
-if (mission === 'learn') {
+if (mission === "learn") {
     console.log('Time to write some code!');
 } else {
     console.log(`Is ${mission} really more fun?`);
